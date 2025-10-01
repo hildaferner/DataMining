128e5e3fc2a017eeb1d1a32f0a26fc9418ea3d5a
Izabellenorlander <izabelle.norlanderr@gmail.com>
2025-10-01 10:07:51 +0200

    filtered alcohol

--- DM_Project/src/exploration.py
+++ DM_Project/src/exploration.py
@@ -1,18 +1,28 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
 
 alcohol = pd.read_excel("data/alcohol.xlsx")  
 #print(alcohol.head())
 #print(alcohol.info())
 
-'''policies = pd.read_excel("data/alcohol_policies.xlsx")
-print(policies.head())'''
+alcohol_filtered = alcohol[
+    alcohol["indicator_name"] == "Alcohol, total per capita (15+) consumption (SDG Indicator 3.5.2) (in litres of pure alcohol)"
+]
+
+# bara könsspecifika
+alcohol_filtered = alcohol_filtered[alcohol_filtered["subgroup"].isin(["Male", "Female"])]
+
+
+print(alcohol_filtered[["setting", "date", "subgroup", "estimate"]].head())
+
 
 mpi = pd.read_excel("data/mpi.xlsx")
 #print(mpi.head())
 
-merged = alcohol.merge(mpi, on=["setting", "date"], how="inner")
+merged = alcohol_filtered.merge(mpi, on=["setting", "date"], how="inner")
 
 '''print(merged.head())
 print(merged.info())'''
@@ -40,13 +50,16 @@ plt.show()
 
 pivot["diff_male_female"] = pivot["Male"] - pivot["Female"]
 
+
+
 avg_diff = pivot.groupby("setting")["diff_male_female"].mean().sort_values()
 
+plt.figure(figsize=(8, 20)) 
 plt.barh(avg_diff.index, avg_diff.values)
 plt.axvline(0, color="black", linewidth=1)
 plt.title("Average gender difference in alcohol consumption")
 plt.xlabel("Difference (Male - Female)")
 plt.ylabel("Country")
+plt.tight_layout()  # gör så att etiketter inte klipps av
 plt.show()
-
 
